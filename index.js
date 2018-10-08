e06bd66f0ad42724d3bcb93f23924f11ca45ed8b
wirus15 <wirus15@gmail.com>
2018-10-08 12:11:32 +0200

    dodany config.dist, zaczety filtr daty, uproszczony filtr hasImg

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ const { JSDOM } = jsdom;
 JSDOM.fromURL(`https://www.wykop.pl/tag/wpisy/${config.tag}/`).then((result) => {
     let entries;
     const getPoints = (node) => node.querySelector('p.vC').getAttribute('data-vc');
-    const hasImg = (node) => {
-        const mediaContent = node.querySelector('.media-content');
-
-        if (!Boolean(mediaContent)) {
-            return false;
-        }
-
-        return Boolean(mediaContent.querySelector('a[href$=".jpg"],a[href$=".png"],a[href$=".gif"]'));
-    };
+    const hasImg = (node) => Boolean(node.querySelector('.media-content > a[href$=".jpg"], .media-content > a[href$=".png"], .media-content > a[href$=".gif"]'));
+    const fromYesterday = (node) => true;
 
     entries = Array.from(result.window.document.querySelectorAll('#itemsStream > .entry.iC'));
     entries = entries.filter(hasImg);
+    entries = entries.filter(fromYesterday);
     entries.sort((a, b) => getPoints(b) - getPoints(a));
 
     const imgUrl = entries[0].querySelector('.media-content > a').getAttribute('href');
