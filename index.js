77abeaf7120aba45c191174cf4e53bf46cc9d76f
wirus15 <wirus15@gmail.com>
2019-05-08 13:15:55 +0200

    removed date filter

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const moment = require('moment');
 const {ajax} = require('rxjs/ajax');
 const {from} = require('rxjs');
 const {map, switchMap, filter, reduce} = require('rxjs/operators');
@@ -6,13 +5,11 @@ const {XMLHttpRequest} = require('xmlhttprequest');
 
 const config = require('./config');
 const slack = require('./slack')(config);
-const yesterday = moment().subtract(1 ,'day');
 const url = `https://a2.wykop.pl/Tags/Entries/${config.tag}/page/1/appkey/${config.wykopApiKey}`;
 const createXHR = () => new XMLHttpRequest();
 
 ajax({url, createXHR}).pipe(
     switchMap(response => from(response.response.data)),
-    filter(entry => moment(entry.date).isSame(yesterday, 'day')),
     filter(entry => entry.embed && entry.embed.type === 'image'),
     reduce((current, next) => !current || next.vote_count > current.vote_count ? next : current),
     map(entry => entry.embed.url)
